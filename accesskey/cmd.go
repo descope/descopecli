ff7540e0ece18ef8a250a2c5d90308f31b4292a9
Gil Shapira <gshapira@gmail.com>
2024-08-07 20:51:26 +0300

    Bump version to v0.8.7 (#43)

--- accesskey/cmd.go
+++ accesskey/cmd.go
@@ -6,20 +6,21 @@ import (
 )
 
 var Flags struct {
-	Name    string
-	Expires int64
-	UserId  string
-	Tenants []string
+	Description string
+	Expires     int64
+	UserId      string
+	Tenants     []string
 }
 
 func AddCommands(parent *cobra.Command, group *cobra.Group) {
 	accessKey := shared.MakeGroupCommand(group, "access-key", "Commands for creating and managing access keys")
 	parent.AddCommand(accessKey)
 
-	shared.AddCommand(accessKey, Create, "create <name> [-e time] [-t tenants] [-u userId]", "Create a new access key", func(cmd *cobra.Command) {
+	shared.AddCommand(accessKey, Create, "create <name> [-d description] [-e time] [-t tenants] [-u userId]", "Create a new access key", func(cmd *cobra.Command) {
 		cmd.Args = cobra.ExactArgs(1)
+		cmd.Flags().StringVarP(&Flags.Description, "description", "d", "", "an optional description for the access key")
 		cmd.Flags().Int64VarP(&Flags.Expires, "expires", "e", 0, "the access key's expiry time (unix time in seconds, default 0 to not expire)")
-		cmd.Flags().StringSliceVarP(&Flags.Tenants, "tenants", "t", nil, "a comma separated list of tenant ids for the user")
+		cmd.Flags().StringSliceVarP(&Flags.Tenants, "tenants", "t", nil, "a comma separated list of tenant ids for the access key")
 		cmd.Flags().StringVarP(&Flags.UserId, "userId", "u", "", "an optional user id to adopt authorizations from")
 	})
 
